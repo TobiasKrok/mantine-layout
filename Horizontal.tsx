8bab7538f0844655982275abc0d91fb4a55ea900
Tobias Kroknes Kristiansen <35228879+TobiasKrok@users.noreply.github.com>
2023-09-24 23:16:06 +0200

    Add files via upload

---
+++ Horizontal.tsx
@@ -0,0 +1,33 @@
+import { Stack, StackProps } from "@mantine/core";
+import React, { forwardRef } from "react";
+import css from "./Layout.module.css";
+import classNames from "classnames";
+
+type HorizontalProps = React.ComponentPropsWithRef<"div"> &
+  StackProps & {
+    center?: boolean;
+    fullWidth?: boolean;
+    fullHeight?: boolean;
+    debug?: boolean;
+  };
+
+const Horizontal = forwardRef<HTMLDivElement, React.ComponentPropsWithRef<"div"> & HorizontalProps>(
+  (props, ref) => {
+    return (
+      <Stack
+        {...props}
+        ref={ref}
+        className={classNames({
+          [css.fullWidth as string]: props.fullWidth,
+          [css.fullHeight as string]: props.fullHeight,
+          [css.center as string]: props.center,
+          [css.debug as string]: props.debug,
+        })}
+      >
+        {props.children}
+      </Stack>
+    );
+  }
+);
+
+export default Horizontal;
---
+++ Vertical.tsx
@@ -0,0 +1,33 @@
+import { Stack, StackProps } from "@mantine/core";
+import React, { forwardRef } from "react";
+import css from "./Layout.module.css";
+import classNames from "classnames";
+
+type VerticalProps = React.ComponentPropsWithRef<"div"> &
+  StackProps & {
+    center?: boolean;
+    fullWidth?: boolean;
+    fullHeight?: boolean;
+    debug?: boolean;
+  };
+
+const Vertical = forwardRef<HTMLDivElement, React.ComponentPropsWithRef<"div"> & VerticalProps>(
+  (props, ref) => {
+    return (
+      <Stack
+        {...props}
+        ref={ref}
+        className={classNames({
+          [css.fullWidth as string]: props.fullWidth,
+          [css.fullHeight as string]: props.fullHeight,
+          [css.center as string]: props.center,
+          [css.debug as string]: props.debug,
+        })}
+      >
+        {props.children}
+      </Stack>
+    );
+  }
+);
+
+export default Vertical;
